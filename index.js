523495b2196b615763f3ed8cdaf7d5fb999b4a3f
omer rokach <77382841+omerokach@users.noreply.github.com>
2021-02-24 17:29:10 +0200

    delete test

--- index.js
+++ index.js
@@ -1,148 +1,159 @@
-const uuid = require('uuid');
-const express = require('express');
-const fs = require('fs');
-const delay = require('express-delay');
+const uuid = require("uuid");
+const express = require("express");
+const fs = require("fs");
+const delay = require("express-delay");
 const app = express();
-const PORT =3000;
+const PORT = 3000;
 app.use(express.json());
 app.use(delay(400));
- // get all the objects
-app.get('/b', (req, res) => {
-    let files = [];
-    const objects = fs.readdirSync('./database');
-    if(!req.params.id.match(/[a-zA-Z0-9]/)){
-        return res.status(400).send({
-            message: "Invalid Bin Id provided"
-        });
+// get all the objects
+app.get("/b", (req, res) => {
+  let files = [];
+  const objects = fs.readdirSync("./database");
+  if (!req.params.id.match(/[a-zA-Z0-9]/)) {
+    return res.status(400).send({
+      message: "Invalid Bin Id provided",
+    });
+  }
+  if (objects.length === 0) {
+    res.send("you have no objects");
+  } else {
+    try {
+      for (let object of objects) {
+        files.push(JSON.parse(fs.readFileSync(`./database/${object}`)));
+      }
+      const successMessage = {
+        success: true,
+        data: files,
+        version: 1,
+      };
+      res.status(200).send(successMessage);
+    } catch (error) {
+      res.status(500).send("there is a problem with the server " + error);
     }
-    if(objects.length === 0){
-        res.send('you have no objects');
-    } else{
-            try {
-                for( let object of objects){
-                    files.push(JSON.parse(fs.readFileSync(`./database/${object}`)));
-                }
-                const successMessage = {
-                    success: true,
-                    data: files,
-                    "version": 1,
-                }
-                res.status(200).send(successMessage);
-            } catch (error) {
-                res.status(500).send('there is a problem with the server ' + error);
-            }
-        }
+  }
 });
 
 // get a specific object by id
-app.get('/b/:id', (req,res) => {
-    if(!req.params.id.match(/[a-zA-Z0-9]/)){
-        return res.status(400).send({
-            message: "Invalid Bin Id provided"
-        });
-    }
-    if(!fs.existsSync(`./database/${req.params.id}.json`)){
-        return res.status(404).send(`{
+app.get("/b/:id", (req, res) => {
+  if (!req.params.id.match(/[a-zA-Z0-9]/)) {
+    return res.status(400).send({
+      message: "Invalid Bin Id provided",
+    });
+  }
+  if (!fs.existsSync(`./database/${req.params.id}.json`)) {
+    return res.status(404).send(`{
             "message": "bin not found"
         }`);
-    }else {
-        fs.readFile(`./database/${req.params.id}.json` ,(err, data) =>{
-        if(err){
-            res.status(500).send('there is a problem with the server '+ err)
-        } else{
-            const body = JSON.parse(data);
-            const successMessage = {
-                success: true,
-                data: body,
-                "version": 1,
-                "parentId": req.params.id
-            }
-            
-            res.status(200).send(successMessage);
-        }
-    })
-    };
+  } else {
+    fs.readFile(`./database/${req.params.id}.json`, (err, data) => {
+      if (err) {
+        res.status(500).send("there is a problem with the server " + err);
+      } else {
+        const body = JSON.parse(data);
+        const successMessage = {
+          success: true,
+          data: body,
+          version: 1,
+          parentId: req.params.id,
+        };
+
+        res.status(200).send(successMessage);
+      }
+    });
+  }
 });
 
 //update specific object by id
-app.put('/b/:id', (req, res) =>{
-    const {body} = req;
-    body.id = req.params.id;
-    if(!req.params.id.match(/[a-zA-Z0-9]/)){
-        return res.status(400).send({
-            message: "Invalid Bin Id provided"
-        });
-    }
-    if(!fs.existsSync(`./database/${req.params.id}.json`)){
-        res.status(404).send(`{
+app.put("/b/:id", (req, res) => {
+  const { body } = req;
+  body.id = req.params.id;
+  if (!req.params.id.match(/[a-zA-Z0-9]/)) {
+    return res.status(400).send({
+      message: "Invalid Bin Id provided",
+    });
+  }
+  if (!fs.existsSync(`./database/${req.params.id}.json`)) {
+    res.status(404).send(`{
             "message": "Bid id not found"
         }`);
-    } else {
-        fs.writeFile(`./database/${req.params.id}.json`, JSON.stringify(body, null, 4), (err) =>{
-        if(err){
-            res.status(500).send('there is a problem with the server '+ err);
-        } else{
-            const successMessage = {
-                success: true,
-                data: body,
-                "version": 1,
-                "parentId": req.params.id
-            }
-            res.status(200).send(successMessage);
+  } else {
+    fs.writeFile(
+      `./database/${req.params.id}.json`,
+      JSON.stringify(body, null, 4),
+      (err) => {
+        if (err) {
+          res.status(500).send("there is a problem with the server " + err);
+        } else {
+          const successMessage = {
+            success: true,
+            data: body,
+            version: 1,
+            parentId: req.params.id,
+          };
+          res.status(200).send(successMessage);
         }
-    })
-    };
+      }
+    );
+  }
 });
 
 //creating new file with new object
-app.post('/b', (req, res) =>{
-    const {body} = req;
-    const id = uuid.v4();
-    if(Object.keys(body).length === 0){
-        res.status(400).send(`{
+app.post("/b", (req, res) => {
+  const { body } = req;
+  const id = uuid.v4();
+  if (Object.keys(body).length === 0) {
+    res.status(400).send(`{
             "message": "Bin cannot be blank"
-        }`)
+        }`);
+  }
+  body.id = id;
+  fs.writeFile(
+    `./database/${id}.json`,
+    JSON.stringify(body, null, 4),
+    (err) => {
+      if (err) {
+        res.status(500).send("there is a problem with the server " + err);
+      } else {
+        const successMessage = {
+          success: true,
+          data: body,
+          version: 1,
+          parentId: id,
+        };
+
+        res.status(200).send(successMessage);
+      }
     }
-    body.id = id;
-    fs.writeFile(`./database/${id}.json`, JSON.stringify(body, null, 4), (err) =>{
-        if(err){
-            res.status(500).send("there is a problem with the server "+err)
-        } else{
-            const successMessage = {
-                success: true,
-                data: body,
-                "version": 1,
-                "parentId": id
-            }
-            
-            res.status(200).send(successMessage);
-        }
-    });
+  );
 });
 
 //deleting specific file by id
-app.delete('/b/:id', (req, res) =>{
-    const id = req.params.id;
-    if(!req.params.id.match(/[a-zA-Z0-9]/)){
-        return res.status(400).send({
-            message: "Invalid Bin Id provided"
-        });
-    }
-    if(!fs.existsSync(`./database/${req.params.id}.json`)){
-        res.status(404).send(`{
-            "message": "Bid id not found"
-        }`);
-    } else {
-        fs.unlink(`./database/${id}.json`, (err) =>{
-            if(err){
-                res.status(500).send('there is a problem with the server '+err);
-            } else{
-                res.status(200).send('success');
-            }
-        } );
-     } } );    
-    
-   
- module.exports = app;
- app.listen(PORT);
- console.log(`listening to ${PORT}`);
+app.delete("/b/:id", (req, res) => {
+  const id = req.params.id;
+  if (!id.match(/[a-zA-Z0-9]/)) {
+    return res.status(400).send({
+      message: "Invalid Bin Id provided",
+    });
+  }
+  if (!fs.existsSync(`./database/${id}.json`)) {
+    return res.status(404).json({
+      message: "Bid id not found",
+    });
+  } else {
+    fs.unlink(`./database/${id}.json`, (err) => {
+      if (err) {
+        return res
+          .status(500)
+          .send("there is a problem with the server " + err);
+      } else {
+        return res.status(200).send("success");
+      }
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`listening to ${PORT}`);
+});
+module.exports = app;
--- index.test.js
+++ index.test.js
@@ -1,43 +1,35 @@
 const request = require("supertest");
-const app = require("./index.js");
+const app = require("./index");
 const fs = require("fs");
-beforeAll(() => {
-  fs.appendFileSync("./database/test1.json", 'Hello content!');
-});
 afterAll(() => {
-  const files = fs.readdirSync("./database");
-  for(let i =0; i<files.length; i++){
-    if(files[i] !== "0ca801d3-cb6d-4850-800b-106644034807.json" && files[i] !== "test.json"){
-      fs.unlinkSync(`./database/${files[i]}`)
-    }
-  }
-  
+  fs.appendFileSync("./database/test1.json", "test!");
 });
 
 describe("GET route", () => {
   const body = {
-      "my-todo": [
-    {
-        "text": "omer",
-        "date": "2021-02-21 10:28:43",
-        "priority": "1"
-    } ],
-    "id": "test"
-}
+    "my-todo": [
+      {
+        text: "omer",
+        date: "2021-02-21 10:28:43",
+        priority: "1",
+      },
+    ],
+    id: "test",
+  };
   const expectedRes = {
-      success: true,
-      data: body,
-      "version": 1,
-      "parentId": "test"
+    success: true,
+    data: body,
+    version: 1,
+    parentId: "test",
   };
 
   const expectedError = {
-      message: "Invalid Bin Id provided"
+    message: "Invalid Bin Id provided",
   };
   const expectedInvalidId = {
-      message: "bin not found"
+    message: "bin not found",
   };
-  
+
   it("Should return a bin by a given id", async () => {
     const response = await request(app).get("/b/test");
     // Is the status code 200
@@ -54,67 +46,74 @@ describe("GET route", () => {
     expect(response.status).toBe(400);
     // Is success:false
     expect(response.body.success).toBe(expectedError.success);
-
   });
   it("Should return an  error message with status code 404 for bin not found", async () => {
-      const response = await request(app).get("/b/0c7");
-      // Is the status code 400
-      expect(response.status).toBe(404);
-      // Is success:false
-      expect(response.body.success).toBe(expectedError.success);
-  
-    });
+    const response = await request(app).get("/b/0c7");
+    // Is the status code 400
+    expect(response.status).toBe(404);
+    // Is success:false
+    expect(response.body.success).toBe(expectedError.success);
+  });
 });
-
 
 describe("PUT route", () => {
   const taskToPost = {
     "my-todo": [
       {
-        "text": "omer",
-        "date": "2021-02-21 10:28:43",
-        "priority": "1"
-      } ],
-      "id": "10"
-    }
-    const files = ["0ca801d3-cb6d-4850-800b-106644034807.json", "test.json", "test1.json"];
-    it("Should update bin successfully by given id", async () => {
-      const response = await request(app).put("/b/test").send(taskToPost);
-      expect(response.status).toBe(200);
-    });
-    
-    it("should not creat new bin when updating", async () =>{
-      const afterPut = fs.readdirSync("./database");
-      expect(afterPut).toEqual(files);
-    });
-    
-    it("should error if invalid id", async () =>{
-      const response = await request(app).put("/b/*").send(taskToPost);
-      expect(response.status).toBe(400);
-      expect(response.body.message).toBe("Invalid Bin Id provided")
-    });
-    
-    
+        text: "omer",
+        date: "2021-02-21 10:28:43",
+        priority: "1",
+      },
+    ],
+    id: "10",
+  };
+  const files = [
+    "0ca801d3-cb6d-4850-800b-106644034807.json",
+    "test.json",
+    "test1.json",
+  ];
+  it("Should update bin successfully by given id", async () => {
+    const response = await request(app).put("/b/test").send(taskToPost);
+    expect(response.status).toBe(200);
   });
-  
-  describe("POST route", () => {
-    const taskToPost = {
-      "my-todo": []
-    };
-    it("Should post a new bin successfully", async () => {
-      const response = await request(app).post("/b").send(taskToPost);
-      expect(response.status).toBe(200);
-      await request(app).get(`./database/${response.body.data.id}`).expect(200);
-    });
+
+  it("should not creat new bin when updating", async () => {
+    const afterPut = fs.readdirSync("./database");
+    expect(afterPut).toEqual(files);
   });
-  
-  
-  describe("DELETE route", () =>{
-      it("Should delete bin by given id", async () => {
-          const response = await request(app).delete("/b/test1.json");
-          const files = fs.readdirSync("./database");
-          expect(response.status).toBe(200);
-          expect(files).not.toMatch("test1.json");
-        await request(app).get(`./database/${response.body.data.id}`).expect(400);
-      })
-    })
+
+  it("should error if invalid id", async () => {
+    const response = await request(app).put("/b/*").send(taskToPost);
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBe("Invalid Bin Id provided");
+  });
+});
+
+// describe("POST route", () => {
+//   afterAll(() => {
+//     const files = fs.readdirSync("./database");
+//     for(let i =0; i<files.length; i++){
+//       if(files[i] !== "0ca801d3-cb6d-4850-800b-106644034807.json" && files[i] !== "test.json"){
+//         fs.unlinkSync(`./database/${files[i]}`)
+//       }
+//     }
+
+//   });
+//   const taskToPost = {
+//     "my-todo": []
+//   };
+//   it("Should post a new bin successfully", async () => {
+//     const response = await request(app).post("/b").send(taskToPost);
+//     expect(response.status).toBe(200);
+//     await request(app).get(`./database/${response.body.data.id}`).expect(200);
+//   });
+// });
+
+describe("DELETE route", () => {
+  it("Should delete bin by given id", async () => {
+    const response = await request(app).delete("/b/test1").expect(200).and(done);
+    console.log(response);
+    expect(response.status).toBe(200);
+    // await request(app).get(`./database/${response.body.data.id}`).expect(400);
+  });
+});
